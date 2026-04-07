c7547b73ea030c6f4a08260638787917270b1bbd
udhayakumar2010 <udhayakuma9554@gmail.com>
2026-04-07 11:41:45 +0530

    Updated App.jsx

--- frontend/ecommerce-frontend/src/App.jsx
+++ frontend/ecommerce-frontend/src/App.jsx
@@ -4,6 +4,7 @@ import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import RegisterPage from './pages/RegisterPage'
 
+
 function App() {
   return (
     <AuthProvider>
@@ -12,6 +13,7 @@ function App() {
           <Route path="/"         element={<HomePage />} />
           <Route path="/login"    element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
+          
         </Routes>
       </BrowserRouter>
     </AuthProvider>
